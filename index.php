5096561d477c3840d9e1921c6b37a58e2d0b4f92
Unknown <laurianepierre@outlook.fr>
2017-01-30 16:21:51 +0100

    avancée du html et du css

--- index.php
+++ index.php
@@ -50,8 +50,10 @@
     <main>
         <div class="portfolio">
             <div class="container">
-                <h4>Portfolio</h4>
-                <h5>Glamour fashion personal street portrait</h5>
+                <div class="titres_portfolio">
+                    <h4>Portfolio/</h4>
+                    <h5>Glamour fashion personal street portrait</h5>
+                </div>
                 <div class="row">
                     <img src="images/img_1_portfolio.png" alt="" class="col-md-6 images">
                     <img src="images/img_1_portfolio.png" alt="" class="col-md-6 images">
@@ -85,19 +87,111 @@
             </div>
         </div>
         <div class="citation">
-
+            <div class="container">
+                <span class="quotation_mark1">“</span>
+                <p class="quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>Aenan vel diam non neque iaculis sollicitudin.</p>
+                <span class="quotation_mark2">”</span>
+                <p class="author">-Lorem Ipsum-</p>
+            </div>
         </div>
         <div class="news">
-
+            <div class="container">
+                <h6>News/</h6>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-6 date">
+                        23 Septembre 2013
+                    </div>
+                    <div class="col-md-6 tags_article">
+                        <img src="" alt="">
+                        <p class="tags_attribues">Glamour, Fashion</p>
+                    </div>
+                </div>
+                <p class="titre_article">Duis dapibus aliquam mi, eget euismod sem scelerisque ut.</p>
+                <img src="images/img_1_portfolio.png" alt="" class="img_article">
+                <p class="article">Nunc nec massa et risus iaculis interdum et sed mi. Nam viverra
+                ac neque eu aliquet. Maecenas posuere, neque non vestibulum
+                sodales, arcu diam interdum nulla, in molestie magna magna vitae
+                justo. Aenean semper volutpat sapien eu porta. Etiam vestibulum
+                pharetra risus vel ultricies.</p>
+                <a href="" class="read_more">Read more... <img src="" alt="flèche"></a>
+            </div>
+            <div class="col-md-6">
+                <p class="titre_tag">Tags</p>
+                <div class="row">
+                    <div class="tags">Glamour</div>
+                    <div class="tags">Street</div>
+                    <div class="tags">Personal</div>
+                    <div class="tags">Portrait</div>
+                    <div class="tags">B&W</div>
+                    <div class="tags">Nude</div>
+                    <div class="tags">Wedding</div>
+                    <div class="tags">Fashion</div>
+                    <div class="tags">Kids</div>
+                </div>
+                <div class="from">from the <span class="blog">blog</span></div>
+                <div class="extrait_article">
+                    <img src="" alt="" class="img_ext">
+                    <p class="resume">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Morbi blandit velit id dolor fermentum,
+                        non posuere libero posuere.</p>
+                    <p class="date_res">26 Aug, 2013</p>
+                </div>
+                <div class="extrait_article">
+                    <img src="" alt="" class="img_ext">
+                    <p class="resume">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Morbi blandit velit id dolor fermentum,
+                        non posuere libero posuere.</p>
+                    <p class="date_res">26 Aug, 2013</p>
+                </div>
+            </div>
         </div>
         <div class="sponsor">
-
+            <div class="container">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+            </div>
         </div>
         <div class="bio">
-
+            <div class="container">
+                <h6>Bio/</h6>
+                <div class="col-md-6">
+                    <h5></h5>
+                    <img src="" alt="" class="avatar">
+                    <div class="img_reseaux">
+                        <img src="images/instagram.png" alt="" class="reseaux">
+                        <img src="images/facebook.png" alt="" class="reseaux">
+                        <img src="images/icon_bizarre.png" alt="" class="reseaux">
+                        <img src="images/twitter.png" alt="" class="reseaux">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <p class="best">Best solution is the simplest idea!</p>
+                    <p class="explain">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel
+                    diam non neque iaculis sollicitudin. Nunc nec massa et risus
+                    iaculis interdum et sed mi. Nam viverra ac neque eu aliquet.
+                    Maecenas posuere, neque non vestibulum sodales, arcu diam
+                    interdum nulla, in molestie magna magna vitae justo. Aenean
+                    semper volutpat sapien eu porta. Etiam vestibulum pharetra
+                    risus vel ultricies. Phasellus tempus, enim sed gravida interdum,
+                    lorem ipsum auctor libero, ut ultrices dolor dolor at nulla.
+                    Integer viverra vitae velit eget tincidunt. Fusce tristique eros ac
+                    est mattis consequat.</p>
+                    <p class="explain">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel
+                    diam non neque iaculis sollicitudin. Nunc nec massa et risus
+                    iaculis interdum et sed mi. Nam viverra ac neque eu aliquet.
+                    Maecenas posuere, neque non vestibulum sodales, arcu diam
+                    interdum nulla, in molestie magna magna vitae justo.</p>
+                </div>
+            </div>
         </div>
     </main>
-    <footer></footer>
+    <footer>
+        <div class="container">
+            <h6>Contact/</h6>
+        </div>
+    </footer>
 
 </body>
 
