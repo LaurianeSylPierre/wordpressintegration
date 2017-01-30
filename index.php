dc11f999f78cffa1aab5c255a3e6407bdb1f695a
Unknown <laurianepierre@outlook.fr>
2017-01-30 10:57:54 +0100

    Ajout des images et du css

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 
@@ -13,14 +15,20 @@
 
     <header>
         <div class="container">
-            <div class="fond"></div>
-            <nav>
+            <div class="fond">
+                <div>
+                    <h1>.Folio</h1>
+                    <h2>One page PSD theme</h2>
+                    <h3>Simple, clear, minimal portfolio template</h3>
+                </div>
+            </div>
+            <nav class="nav_header">
                 <ul>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
-                    <li><a href=""></a></li>
+                    <li><a href="">.Folio</a></li>
+                    <li><a href="">Work</a></li>
+                    <li><a href="">News</a></li>
+                    <li><a href="">Bio</a></li>
+                    <li><a href="">Contact</a></li>
                     <li><a href=""></a></li>
                     <li><a href=""></a></li>
                     <li><a href=""></a></li>
@@ -29,7 +37,50 @@
             </nav>
         </div>
     </header>
-    <main></main>
+    <main>
+        <div class="container">
+            <div class="portfolio">
+                <h4>Portfolio</h4>
+                <h5>Glamour fashion personal street portrait</h5>
+                <div class="row">
+                    <img src="images/img_1_portfolio.png" alt="" class="col-md-6 images">
+                    <img src="images/img_1_portfolio.png" alt="" class="col-md-6 images">
+                </div>
+                <div class="row">
+                    <img src="images/img_1_portfolio.png" alt="" class="col-md-6 images">
+                    <img src="images/img_1_portfolio.png" alt="" class="col-md-6 images">
+                </div>
+                <div class="row">
+                    <img src="images/img_2_portfolio.png" alt="" class="col-md-3 images">
+                    <img src="images/img_2_portfolio.png" alt="" class="col-md-3 images">
+                    <img src="images/img_2_portfolio.png" alt="" class="col-md-3 images">
+                    <img src="images/img_2_portfolio.png" alt="" class="col-md-3 images">
+                </div>
+                <div class="row">
+                    <img src="images/01.png" alt="" class="col-md-3 images">
+                    <img src="images/02.png" alt="" class="col-md-3 images">
+                    <img src="images/03.png" alt="" class="col-md-3 images">
+                    <img src="images/04.png" alt="" class="col-md-3 images">
+                </div>
+                <div class="row">
+                    <img src="images/05.png" alt="" class="col-md-6 images">
+                    <img src="images/06.png" alt="" class="col-md-3 images">
+                    <img src="images/07.png" alt="" class="col-md-3 images">
+                </div>
+                <div class="row">
+                    <img src="images/08.png" alt="" class="col-md-3 images">
+                    <img src="images/09.png" alt="" class="col-md-3 images">
+                    <img src="images/10.png" alt="" class="col-md-6 images">
+                </div>
+            </div>
+            <div class="citation">
+
+            </div>
+            <div class="news"></div>
+            <div class="sponsor"></div>
+            <div class="bio"></div>
+        </div>
+    </main>
     <footer></footer>
 
 </body>
